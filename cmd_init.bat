2e8046b1d717b6a423d2ee95bf4751e78bd5c3b2
ganemaruko <kazuho@hacarus.com>
2021-04-16 10:35:11 +0900

    openの変更

--- cmd_init.bat
+++ cmd_init.bat
@@ -14,7 +14,7 @@ doskey mv=move $*
 doskey touch=type nul $g $*
 doskey rm=if exist $*\nul (rd /s /q $*) else (del /q $*)
 doskey ifconfig=ipconfig
-doskey open=start
+doskey open=explorer
 doskey ps=tasklist $*
 doskey kill=Taskkill /PID $* /F
 cd C:\Users\kogan
